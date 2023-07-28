930d5ce6abe54faafc10bd59ea95ef2df0f11cb8
David Slavík <centroprint@email.cz>
2023-07-28 12:57:36 +0200

    Konec videa 70

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
 // Základní proměnné
 let totalScore, roundScore, activePlayer, dice;
-
+/* 
 totalScore = [0,0];     // pole 0, 1
 roundScore = 0;
 activePlayer = 0;   // 0 = první hráč, 1 = druhý hráč
-
-
+ */
+/* 
 // vynulování a odstranění kostky
 document.getElementById("totalScorePlayer-0").textContent = 0;
 document.getElementById("totalScorePlayer-1").textContent = 0;
 document.getElementById("currentScore-0").textContent = 0;
 document.getElementById("currentScore-1").textContent = 0;
-
+ */
+/* 
 document.querySelector(".diceImage").style.display = "none";
+ */
+
+newStart();
+
+// Nová hra - reset hry
+function newStart(){
+    totalScore = [0,0];     // pole 0, 1
+    roundScore = 0;
+    activePlayer = 0;   // 0 = první hráč, 1 = druhý hráč
+
+    // vynulování a odstranění kostky
+    document.getElementById("totalScorePlayer-0").textContent = 0;
+    document.getElementById("totalScorePlayer-1").textContent = 0;
+    document.getElementById("currentScore-0").textContent = 0;
+    document.getElementById("currentScore-1").textContent = 0;
+
+    // skrytí kostky
+    document.querySelector(".diceImage").style.display = "none";
+
+    // Texty do původního stavu
+    document.querySelector("#name-0").textContent = "Skóre 1. hráče";
+    document.querySelector("#name-1").textContent = "Skóre 2. hráče";
+
+    // Vrátíme zvýraznění aktivního hráče k prvnímu a u druhého odstraníme
+    document.querySelector(".totalScore0").classList.add("active"); 
+    document.querySelector(".totalScore1").classList.remove("active"); 
+}
+
+
+
+
+
+
 
 // měníme obrázek kostky podle náhodného čísla
 document.querySelector(".rollDice").addEventListener("click", function() {
@@ -68,7 +102,7 @@ document.querySelector(".holdScore").addEventListener("click", function(){
     }
 })
 
-
+document.querySelector(".newGame").addEventListener("click", newStart);
 
 
 
